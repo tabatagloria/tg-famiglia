a5e7a352db3663e0a48472af2418866b6165cdd8
Tábata Glória <tabatabruna@yahoo.com.br>
2021-11-16 20:35:13 -0300

    Corrigindo BuscaController.js

--- backend/src/controllers/BuscaController.js
+++ backend/src/controllers/BuscaController.js
@@ -1,16 +1,21 @@
-const { Op } = require('sequelize');
+const { Op, Sequelize} = require('sequelize');
 const Familiar = require('../models/Familiar');
 const Documento = require('../models/Documento');
+
 
 module.exports = {
     async show(req, res) {
         const nome = req.body;
+        nome.toString().toLowerCase();
+
         const familiar = await Familiar.findAll({
-            attribute: [ 'nome' ],
-            where:{
-                nome: {
-                    [Op.like]: `%${req.body.nome}%`
-                  }
+            attribute: [ 'nome', [Sequelize.fn('lower', Sequelize.col('nome')),] ],
+            where: {
+                nome:
+                     {
+                        [Op.like]: `%${req.body.nome}%`
+                    }
+               
             },
             include: [{
                 association: 'documentos'
